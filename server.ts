25eab7f79e80b4a5a5f5557aaaa8f38c1fcdd113
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-05-26 20:06:47 +0200

    feature/templateStrings

--- server.ts
+++ server.ts
@@ -1,13 +1,16 @@
-import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts";
+import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts"
 
-const app = new Application();
-const message = "Hello World,\r Using Deno running inside a docker container ☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕";
+const app = new Application()
+
+const hello = 'Hello World,'
+const about = 'Using Deno running inside a docker container.'
+const unicodes = '☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕'
+
+const message = `${hello}\r${about}\r${unicodes}´
 
 app.use((ctx: any) => {
-  ctx.response.body = message;
-  //console.log(`request received: ${ctx.request.url}`);
-});
+  ctx.response.body = message
+})
 
-const port = Deno.env.get("PORT") || "8080";
-//console.log(`Starting server at port: ${port}`);
-await app.listen({ port: +port });
+const port = Deno.env.get("PORT") || "8080"
+await app.listen({ port: +port })
