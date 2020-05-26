0f8bc68a01ee11e10e261e9f4318b0d6dd523d3d
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-05-26 20:14:31 +0200

    fix/error

--- server.ts
+++ server.ts
@@ -1,16 +1,17 @@
-import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts"
+import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts";
 
-const app = new Application()
+const app = new Application();
 
-const hello = 'Hello World,'
-const about = 'Using Deno running inside a docker container.'
-const unicodes = '☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕'
+const hello = 'Hello World,';
+const about = 'Using Deno running inside a docker container.';
+const unicodes = '☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕';
 
-const message = `${hello}\r${about}\r${unicodes}´
+const message = `${hello}\r${about}\r${unicodes}`;
 
 app.use((ctx: any) => {
-  ctx.response.body = message
-})
+  ctx.response.body = message;
+});
 
-const port = Deno.env.get("PORT") || "8080"
-await app.listen({ port: +port })
+const port = Deno.env.get("PORT") || "8080";
+
+await app.listen({ port: +port });
