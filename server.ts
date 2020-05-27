e29d52d40ea27d9048a58ce78304fae1fa11de5d
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-05-27 10:52:31 +0200

    fix/port as number

--- server.ts
+++ server.ts
@@ -1,12 +1,11 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 import router from './routes.ts'
-const port = Number(Deno.env.get("PORT")) || 5000
 
 const app = new Application()
-
 app.use(router.routes())
 app.use(router.allowedMethods())
 
+const port = Deno.env.get("PORT") || "8080";
 console.log(`Server running on port ${port}`)
 
-await app.listen({ port })
+await app.listen({ port: +port });
