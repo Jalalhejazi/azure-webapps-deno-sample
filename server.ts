e6d7fa92b540fd08bc145812a123ebcb71ebbfac
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-05-27 12:17:15 +0200

    Try/routing with oak latest version

--- server.ts
+++ server.ts
@@ -1,23 +1,35 @@
 /*****************************************
-// DRY: Separations
-
-import router from './routes.ts'
-
-const app = new Application()
-app.use(router.routes())
-app.use(router.allowedMethods())
-*******************************************/
+// DevOps: Can Deploy this version to azure
 
 import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts";
-import router                   from './routes.ts';
 import messages                 from './messages.ts';
 
-const app = new Application();
-
+const app = new Application()
 app.use((ctx: any) => {
   ctx.response.body = messages;
 });
+*******************************************/
 
+import { Application, Context } from "https://deno.land/x/oak/mod.ts";
+import router                   from './routes.ts';
+import messages                 from './messages.ts';
+
+
+
+/***********************************************
+Define Routing
+************************************************/
+const app = new Application();
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+
+
+
+
+/***********************************************
+await port listning ........
+************************************************/
 
 const port = Deno.env.get("PORT") || "8080";
 console.log( `Listning on Port ${port} ` );
