188987503b02243d6d789083e168991edf7ebc1e
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-05-27 11:19:01 +0200

    revert/old feature

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+/*
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 import router from './routes.ts'
 
@@ -9,3 +10,24 @@ const port = Deno.env.get("PORT") || "8080";
 console.log(`Server running on port ${port}`)
 
 await app.listen({ port: +port });
+*/
+
+import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts";
+
+const app = new Application();
+
+const hello = 'Hello World,';
+const about = 'Using Deno running inside a docker container.';
+const unicodes = '☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕';
+
+const message = `${hello}\r${about}\r${unicodes}`;
+
+app.use((ctx: any) => {
+  ctx.response.body = message;
+});
+
+const port = Deno.env.get("PORT") || "8080";
+
+await app.listen({ port: +port });
+
+console.log( `Listning on Port ${port} ` );
