d5c0e1383353d51655e80e7e498b5d4ad3c7e9e9
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-05-27 12:03:10 +0200

    Try/only import routes.ts
    experimental if deno can fix peer dependency

--- server.ts
+++ server.ts
@@ -1,27 +1,18 @@
-/*
-import { Application } from 'https://deno.land/x/oak/mod.ts'
+/*****************************************
+// DRY: Separations
+
 import router from './routes.ts'
 
 const app = new Application()
 app.use(router.routes())
 app.use(router.allowedMethods())
-
-const port = Deno.env.get("PORT") || "8080";
-console.log(`Server running on port ${port}`)
-
-await app.listen({ port: +port });
-*/
+*******************************************/
 
 import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts";
+import router                   from './routes.ts';
 import messages                 from './messages.ts';
 
 const app = new Application();
-
-//const hello = 'Hello World,';
-//const about = 'Using Deno running inside a docker container.';
-//const unicodes = '☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕';
-
-//const message = `${hello}\r${about}\r${unicodes}`;
 
 app.use((ctx: any) => {
   ctx.response.body = messages;
