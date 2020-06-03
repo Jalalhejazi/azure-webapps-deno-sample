d797565f8a6c1a96cd5ffd5e70dd6ea71192d668
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-06-03 09:46:52 +0200

    fix/superusers

--- messages.ts
+++ messages.ts
@@ -4,6 +4,7 @@ messages.push("☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕");
 messages.push("DevOps with github actions.");
 messages.push("Deno running inside a docker container.");
 messages.push("☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕");
+messages.push("☁️☁️☁️☁️☁️ kursus hos SuperUsers 2020 ☁️☁️☁️☁️☁️");
 
 export default messages;
 
