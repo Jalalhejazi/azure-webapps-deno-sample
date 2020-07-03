a6d7fa87434b2af7572389d435cf67fa78a6a06e
Jalal Hejazi <Jalal.hejazi@gmail.com>
2020-07-03 09:45:44 +0200

    start/event

--- messages.ts
+++ messages.ts
@@ -5,8 +5,6 @@ messages.push("DevOps with github actions.");
 messages.push("Deno running inside a docker container.");
 messages.push("☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕");
 messages.push("☁️☁️☁️☁️☁️ kursus hos SuperUsers 2020 ☁️☁️☁️☁️☁️");
-messages.push("2020-07-01");
-messages.push("Rasmus og Jalal");
-
+messages.push("2020-07-03");
 export default messages;
 
