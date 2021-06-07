bc3ed0cbca762a874b2351c59e2b7ad5d02b5dba
Jalal Hejazi <Jalal.hejazi@gmail.com>
2021-06-07 22:50:20 +0200

    FIX/date

--- messages.ts
+++ messages.ts
@@ -4,7 +4,7 @@ messages.push("☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕");
 messages.push("DevOps with github actions.");
 messages.push("Deno running inside a docker container.");
 messages.push("☁️🦕☁️🦕☁️🦕☁️☁️🦕🦕🦕");
-messages.push("☁️☁️☁️☁️☁️ kursus hos SuperUsers 2020 ☁️☁️☁️☁️☁️");
-messages.push("2020-09-08 kursus az204");
+messages.push("☁️☁️☁️☁️☁️ kursus hos SuperUsers 2021 ☁️☁️☁️☁️☁️");
+messages.push("2021-06-07 kursus az204");
 export default messages;
 
